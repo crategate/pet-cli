36b4605fd6e596c91121c6450dbb7e3e9b6a64f2
shifkey <sevensidedsilver@gmail.com>
2025-11-03 12:25:23 -0800

    extract to lib

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::error::Error;
-use std::{env, fs, process};
+use std::{env, process};
+
+use pet_cli::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,29 +11,8 @@ fn main() {
     });
     println!("Searching for : {}", config.query);
     println!("in dis file : {}", config.filename);
-    if let Err(e) = run(config) {
+    if let Err(e) = pet_cli::run(config) {
         println!("App Error: {}", e);
         process::exit(1);
-    }
-}
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("with this text: \n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
